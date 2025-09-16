ce1686ba4b61954e58637dce97e50fe0ce963577
Nour AL KOTOB <alkotobnour@gmail.com>
2025-09-16 12:21:05 +0200

    fix "/" support in titles...
    From now on illegal characters will be escaped and swapped in a way
    that allows to display them correctly and write their content in
    correctly-named files.
    
    for example:
    
    \# CI/CD
    will be saved in
    <target>/content/CI```CD

--- do_shuffle.sh
+++ do_shuffle.sh
@@ -5,7 +5,7 @@
 
 [ "$INTERACTIVE_ENABLED" ] && . "$ENTRY/3way.sh"
 
-trap 'exec 3<&- 4<&-' INT TERM EXIT
+trap 'exec 3<&- 4<&- 5<&-' INT TERM EXIT
 
 exec 3<"$TARGET/$SHUFFLES"
 exec 4<"$TARGET/$RENAMES"
@@ -13,5 +13,17 @@ exec 4<"$TARGET/$RENAMES"
 while true; do
   read -r title <&3 || break
   read -r rename <&4 || rename=$title
-  awk -v rename="$rename" 'NR==1{print rename} NR!=1{print}' <"$TARGET/$CONTENT/$title"
+
+  target=$rename
+  while :; do
+    case "$target" in
+    */*)
+      pre=${target%/*}
+      post=${target##*/}
+      target=$pre\`\`\`$post
+      ;;
+    *) break ;;
+    esac
+  done
+  gawk -v rename="$rename" 'NR==1{print rename} NR!=1{print}' <"$TARGET/$CONTENT/$target"
 done
--- parse.sh
+++ parse.sh
@@ -4,15 +4,15 @@ if [ ! -t 0 ]; then
   rm -f "$TARGET/$TITLES"
   rm -rf "$TARGET/$CONTENT"
   mkdir -p "$TARGET/$CONTENT"
-  gawk -v "TARGET=$TARGET" -v "CONTENT=$CONTENT" -v "TITLES=$TITLES" '
+  gawk -v "TARGET=$TARGET" -v "CONTENT=$CONTENT" -v "TARGET_TITLES=$TARGET/$TITLES" '
   /^```/ {
     fenced = 1 - fenced
   }
   /^#/ && !fenced {
     title = $0
-    TARGET_TITLES = TARGET "/" TITLES
+    gsub("/", "```", title)
     TARGET_CONTENT = TARGET "/" CONTENT "/" title
-    print title >> TARGET_TITLES
+    print $0 >> TARGET_TITLES
   }
   {
     print $0 >> TARGET_CONTENT
